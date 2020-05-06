40cb0eadc291d249e74bb3fb01020c2307e7bcf5
cincarnato <cristian.incarnato@sondeos.com.ar>
2020-05-06 18:28:02 -0300

    Form complete!

--- ui/app.js
+++ ui/app.js
@@ -1,58 +1,33 @@
 var app = new Vue({
     el: '#app',
     computed: {
-        getModuleColor() {
-            return (index) => {
-                return (index == this.selected) ? 'grey' : 'transparent'
+        getPropSelected(){
+            if(this.propSelected != null){
+                return this.modules[this.moduleSelected].properties[this.propSelected]
             }
+            return {name:'',type:'', ref:'', label:'',icon:'',required:false,search: false}
         }
     },
+    methods:{
+      apply(property){
+          console.log(property)
+          if(this.moduleSelected !== null && this.modules[this.moduleSelected] !== undefined){
+             let index = this.modules[this.moduleSelected].properties.findIndex(prop => prop.name == property.name)
+              if(index === -1){
+                  this.modules[this.moduleSelected].properties.push(property)
+              }else{
+                  this.modules[this.moduleSelected].properties[index] = property
+              }
+          }
+      }
+    },
     data: {
-        message: 'Hello Vue!',
-        selected: null,
+        moduleSelected: null,
+        propSelected: null,
         modules: [{
-            name: "Demo"
+            name: "Demo",
+            properties: []
         }]
     }
 })
 
-
-Vue.component('formProperty', {
-    data() {
-        return {
-            options: ['String','Int','Float','Date','ObjectId'],
-            form: {
-                name: '',
-                type: '',
-                required: false,
-                search: false
-            }
-        }
-    },
-    template: `<div class="row">
-
-    <div class="col-12 py-1">
-       <label class="px-1">Name: </label><input type="text" v-model="form.name">
-    </div>
-    
-    <div class="col-12 py-1">
-        <label class="px-1">Type: </label>
-        <select type="text" v-model="form.type">
-            <option v-for="(v,i) in options" :value="v" :key="i">{{v}}</option>
-        </select>
-    </div>
-    
-    <div class="col-12 py-1" v-if="form.type =='ObjectId'">
-        <label class="px-1">Ref: </label><input type="text" v-model="form.ref">
-    </div>
-    
-    <div class="col-12 py-1">
-        <label class="px-1">Required: </label><input type="checkbox" v-model="form.required">
-    </div>
-    
-    <div class="col-12 py-1" >
-        <label class="px-1">Use in search: </label><input type="checkbox" v-model="form.search">
-    </div>
-    
-</div>`
-})
---
+++ ui/formProperty.js
@@ -0,0 +1,103 @@
+
+Vue.component('formProperty', {
+    props:{
+        p: {type:Object, default: () => {return {name:'',type:'', ref:'', label:'',icon:'',required:false,search: false} } }
+    },
+    data() {
+        return {
+            options: ['String','Int','Float','Date','ObjectId'],
+            errors:[],
+            form: {
+                name: this.p.name?this.p.name:'',
+                type: this.p.type?this.p.type:'',
+                ref: this.p.ref?this.p.ref:'',
+                label: this.p.label?this.p.label:'',
+                icon: this.p.icon?this.p.icon:'',
+                required: this.p.required?this.p.required:false,
+                search: this.p.search?this.p.search:false
+            }
+        }
+    },
+    watch:{
+        p:{
+            deep:true,
+            handler: function(val){
+                this.form = val
+            }
+        }
+    },
+    methods:{
+        apply(){
+            let error = false
+
+            //Check name required
+            if(this.form.name.length == 0){
+                this.errors.push('name')
+                error = true
+            }else{
+                let index = this.errors.indexOf('name');
+                if (index !== -1) this.errors.splice(index, 1);
+            }
+
+            //Check type required
+            if(this.form.type.length == 0){
+                error = true
+                this.errors.push('type')
+            }else{
+                let index = this.errors.indexOf('type');
+                if (index !== -1) this.errors.splice(index, 1);
+            }
+
+            //Check ref required
+            if(this.form.type == 'ObjectId' && this.form.ref.length == 0){
+                error = true
+                this.errors.push('ref')
+            }else{
+                let index = this.errors.indexOf('ref');
+                if (index !== -1) this.errors.splice(index, 1);
+            }
+
+            if(!error){
+                this.$emit('apply', Object.assign({}, this.form) )
+            }else{
+                console.log('ERROR')
+            }
+        }
+    },
+    template: `<div class="row">
+
+    <div class="col-12 py-1">
+       <label class="px-1">Name:</label><input :class="{'error': errors.includes('name')}" type="text" v-model="form.name">
+    </div>
+    
+    <div class="col-12 py-1">
+        <label class="px-1">Type:</label>
+        <select :class="{'error': errors.includes('name')}" type="text" v-model="form.type">
+            <option v-for="(v,i) in options" :value="v" :key="i">{{v}}</option>
+        </select>
+    </div>
+    
+    <div class="col-12 py-1" v-if="form.type =='ObjectId'">
+        <label class="px-1">Ref: </label><input :class="{'error': errors.includes('ref')}"  type="text" v-model="form.ref">
+    </div>
+    
+    <div class="col-12 py-1">
+       <label class="px-1">Label: </label><input type="text" v-model="form.label">
+    </div>
+    
+    <div class="col-12 py-1">
+       <label class="px-1">Icon: </label><input type="text" v-model="form.icon">
+    </div>
+    
+    <div class="col-12 py-1">
+        <label class="px-1">Required: </label><input type="checkbox" v-model="form.required">
+    </div>
+    
+    <div class="col-12 py-1" >
+        <label class="px-1">Use in search: </label><input type="checkbox" v-model="form.search">
+    </div>
+     <div class="col-12 py-1" >
+        <button @click="apply">Apply</button>
+     </div>
+</div>`
+})
